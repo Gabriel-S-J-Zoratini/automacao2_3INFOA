c5f53086c7d1b7fd48e4732334b140e0d4976c30
Gabriel S J Zoratini <gabriel.zoratini@alunos.ifsuldeminas.edu.br>
2024-03-19 16:57:36 -0300

    Aula5

---
+++ semana5/mouse_ex2.py
@@ -0,0 +1,5 @@
+import pyautogui
+#coloca o mouse na imagem
+pyautogui.moveTo(188,232, duration=0.2)
+#arrasta a imagem para a pasta
+pyautogui.dragRel(255,0, duration=0.2)
---
+++ semana5/teclado1.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+pyautogui.click(880,500, duration=0.2)
+pyautogui.write('2995981', interval=0.5)
+pyautogui.click(880,600, duration=0.2)
+pyautogui.write('1111111', interval=0.5)
+pyautogui.click(880,682, duration=0.2)
